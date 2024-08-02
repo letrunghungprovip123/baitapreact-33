a28629114852f6709aee0c27c2002907c59ecce0
letrunghungprovip123 <hle01678z@gmail.com>
2024-08-02 16:24:59 +0700

    update

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,25 +4,6 @@ function Header() {
     <div id="header" className="header">
       <div className="container-fluid pt-lg-2 px-lg-5">
         <div className="header-content d-flex justify-content-between align-items-center py-2">
-          <div className="content-left">
-            <ul>
-              <li>
-                <a href="./index.html">HOME</a>
-              </li>
-              <li>
-                <a href="#">NEW RELEASES</a>
-              </li>
-              <li>
-                <a href="#">BRANDS</a>
-              </li>
-              <li>
-                <a href="#">CATEGORIES</a>
-              </li>
-              <li>
-                <a href="#">SALE</a>
-              </li>
-            </ul>
-          </div>
           <div className="content-center">
             <img src="./src/assets/image/logo.png" alt />
           </div>
